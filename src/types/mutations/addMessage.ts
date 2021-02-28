cb4e0c56966e8b43cdf8ebe319a47262aaa5b616
krunal dodiya <kunal.dodiya1@gmail.com>
2021-02-28 12:39:51 +0530

    refactor to camelcase from snakecase

--- src/types/mutations/addMessage.ts
+++ src/types/mutations/addMessage.ts
@@ -2,17 +2,13 @@ import { intArg, mutationField, nonNull, stringArg } from 'nexus'
 
 export const addMessage = mutationField('addMessage', {
   type: 'Message',
-  args: { receiver_id: nonNull(intArg()), message: nonNull(stringArg()) },
-  resolve: async (
-    parent,
-    { receiver_id, message },
-    { prisma, user, pubsub },
-  ) => {
+  args: { buddy_id: nonNull(intArg()), message: nonNull(stringArg()) },
+  resolve: async (parent, { buddy_id, message }, { prisma, user, pubsub }) => {
     if (!user) throw new Error('Unauthenticated')
 
     const newMessage = await prisma.message.create({
       data: {
-        receiver_id,
+        receiver_id: buddy_id,
         sender_id: user.id,
         message,
       },
